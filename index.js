587c5125cf825285aece4629be2c731e875ed12a
AdrianFdz19 <126997909+AdrianFdz19@users.noreply.github.com>
2024-05-18 22:46:42 -0600

    new

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
+import pkg from 'pg';
+const {Pool} = pkg;
 import { config } from 'dotenv';
 config();
 const PORT = process.env.PORT || 3009;
@@ -8,6 +10,35 @@ const PORT = process.env.PORT || 3009;
 app.use(express.json());
 app.use(cors());
 
+//Conectar a la base de datos
+const isDevServer = false;
+
+export let pool;
+
+if(isDevServer) {
+    pool = new Pool({
+        connectionString: process.env.EXTERNAL_URL,
+        ssl: true
+    });
+} else {
+    pool = new Pool({
+        connectionString: process.env.INTERNAL_URL
+    });
+};
+
 app.get('/', (req,res)=>res.send('Server online'));
+
+app.get('/test-database', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT CURRENT_DATE');
+        const currentDate = result.rows[0].current_date;
+        client.release();
+        res.json({ currentDate });
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.listen(PORT, ()=>console.log('Server on port ', PORT));
